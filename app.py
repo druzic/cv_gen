88318f4840aba60acf91f9a3545964e71f2a53b3
druzic <ruzicdominik11@gmail.com>
2025-03-11 14:17:51 +0100

    added grok(xAI) and groq(llama 3.3)

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
-from openai import OpenAI  # Ispravno uvozimo OpenAI klasu
+from openai import OpenAI
+from groq import Groq
 
 # Učitavamo API ključ iz .env datoteke
 load_dotenv()
 
 # Vraćamo API ključ iz .env datoteke
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+XAI_API_KEY = os.getenv("XAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # Provjeravamo da li je API ključ postavljen
 if not OPENAI_API_KEY:
@@ -15,9 +18,27 @@ if not OPENAI_API_KEY:
 else:
     st.success("OpenAI API ključ je uspješno učitan!")
 
+if not XAI_API_KEY:
+    st.error("Nema API ključa za xAI! Molimo postavite API ključ u .env datoteku.")
+else:
+    st.success("xAI API ključ je uspješno učitan!")
+
+if not GROQ_API_KEY:
+    st.error("Nema API ključa za Groq! Molimo postavite API ključ u .env datoteku.")
+else:
+    st.success("Groq API ključ je uspješno učitan!")
+
 # Inicializiramo OpenAI klijenta
 if OPENAI_API_KEY:
     openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
+client = OpenAI(
+    api_key=XAI_API_KEY,
+    base_url="https://api.x.ai/v1",
+)
+clientGroq = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
 
 st.title("Implementacija konverzacijskog agenta za generiranje životopisa")
 st.write("Streamlit je uspješno pokrenut!")
@@ -25,7 +46,7 @@ st.write("Streamlit je uspješno pokrenut!")
 st.sidebar.title("Postavke")
 models = st.sidebar.radio(
     "Koji model želite koristiti?",
-    ["OpenAI", "GPT-2", "DialoGPT", "Blenderbot"],
+    ["OpenAI", "Groq", "Grok", "Blenderbot"],
     index=None,
 )
 
@@ -49,11 +70,38 @@ def get_response(prompt, model):
         except Exception as e:
             return f"Greška sa OpenAI API: {str(e)}"
 
-    elif model == "GPT-2":
-        return "GPT-2 još nije implementiran."
+    elif model == "Groq":
+        if not GROQ_API_KEY:
+            return "Greška: Groq API ključ nije postavljen."
+        try:
+            print(GROQ_API_KEY)
+            chat_completion = clientGroq.chat.completions.create(
+                messages=[
+                    {"role": "system", "content": "Talk on croatian language. You are a helpful assistant specializing in creating resumes."},
+                    {"role": "user", "content": prompt}
+                ],
+                model="llama-3.3-70b-versatile",
+                )
+            print(chat_completion.choices[0].message.content)
+            return chat_completion.choices[0].message.content
+        except Exception as e:
+            return f"Greška sa Groq API: {str(e)}"
 
-    elif model == "DialoGPT":
-        return "DialoGPT još nije implementiran."
+    elif model == "Grok":
+        if not XAI_API_KEY:
+            return "Greška: xAI API ključ nije postavljen"
+        try:
+            completion = client.chat.completions.create(
+                model="grok-2-latest",
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant specializing in creating resumes."},
+                    {"role": "user", "content": prompt},
+                ],
+            )
+            print(completion.choices[0].message)
+            return completion.choices[0].message.content
+        except Exception as e:
+            return f"Greška sa xAI API: {str(e)}"
 
     elif model == "Blenderbot":
         return "Blenderbot još nije implementiran."
