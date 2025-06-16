9d3ba972ec4279a4c19446f8350c2bcbf50e9d7e
druzic <ruzicdominik11@gmail.com>
2025-06-16 14:48:12 +0200

    better prompt

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 test_cv_answers = [
+    {"role": "user", "content": "Dominik Ružić, adresa Brčanska 7 Vinkovci, email ruzicdominik11@gmail.com, broj mobitela 0998505024, github profil druzic, 2 kvalifikacije imam"
+    "Fakultet informatike u puli prvostupnik informatike 2019 10mj zapoceo a zavrsio 2022 9mj relevantni kolegiji: Osnove IKT, Strukture podataka i algoritma, Baze poda taka, "
+    "Napredne tehnike programiranja, Programsko inženjerstvo, Web aplikacije, druga kvalifikacija isti fakultet ali magistar i zapoceo u 10mj 2022 i zavrsavam u 9mj 2025"
+    "relevantni kolegiji: Raspodijeljeni sustavi, Mobilne aplikacije, Neuronske mreže i duboko učenje, Napredni algoritmi i strukture podataka, Internet stvari,"
+    "2 stručne prakse imam, prva praksa Lescan Digital se zove tamo sam bio wordpress seo administrator od travnja 2022 do svibnja 2022, druga praksa Tri M je tvrtska "
+    "razvoj web cms sustava za administraciju od lipnja 2021 do rujna 2021 nemam opis za obje, napredno vue js, node js, mongodb, firebase,postgresql a ostalo reactjs,"
+    "fastapi,tenstorflow, java i flutter,cpp i git, hrvatski , engleski b2, projekti Web • Fakultetski i privatni projekti web aplikacija, primarno koriˇ stenjem Vue.js i "
+    "MongoDB, te dodatno React i Firebase. Android • Fakultetski projekti razvoja mobilnih aplikacija, uključujući Java aplikaciju za vremensku prognozu te aplikaciju za upravljanje "
+    "slikama koristeći Flutter, FastAPI i PostgreSQL. 3D Igra • Fakultetski grupni projekt razvoja 3D igre koriˇ stenjem Unity game engine-a, interesi "
+    "Web i mobilni razvoj, razvoj videoigara, sport, socijalne vještine voljan pomoći, volim raditi u timu, konstruktivan, uporan, tolerantan"}]
+
+"""
+test_cv_answers = [
     {"role": "user", "content": '{ "full_name": "Dominik Ružić", "address": "Brčanska 7, 32100 Vinkovci, Hrvatska", "email": "ruzicdominik11@gmail.com", '
     '"phone": "0998505024", "github": "https://github.com/druzic", "education": [ { "institution": "Fakultet informatike u Puli", "degree": '
     '"Prvostupnik informatike", "start_date": "Lis 2019", "end_date": "Ruj 2022", "description": "Fakultet informatike u Puli" }, { "institution": '
@@ -11,6 +24,6 @@ test_cv_answers = [
     'privatni projekti web aplikacija, primarno korištenjem Vue.js i MongoDB, te dodatno React i Firebase." }, { "title": "Android", "description": '
     '"Fakultetski projekti razvoja mobilnih aplikacija, uključujući Java aplikaciju za vremensku prognozu te aplikaciju za upravljanje slikama koristeći '
     'Flutter, FastAPI i PostgreSQL." }, { "title": "3D Igra", "description": "Fakultetski grupni projekt razvoja 3D igre korištenjem Unity game engine-a." '
-    '} ], "interests": "Web i mobilni razvoj, razvoj videoigara, sport", social_skills: "Komunikativan, timski igrač, rješavač problema"}' },
-
+    '} ], "interests": "Web i mobilni razvoj, razvoj videoigara, sport", "social_skills": "Komunikativan, timski igrač, rješavač problema"}' },
 ]
+"""
